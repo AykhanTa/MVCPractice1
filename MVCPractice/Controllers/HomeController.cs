594df24b3886cb9d01c6591c3ee99d222ea04e43
AykhanTa <aykhanft@code.edu.az>
2024-07-24 01:27:28 +0400

    feat: "add City Country relation"

--- MVCPractice/Controllers/HomeController.cs
+++ MVCPractice/Controllers/HomeController.cs
@@ -1,24 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
+using MVCPractice.Models;
 
 namespace MVCPractice.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public List<Country> Countries { get; set; } = new()
+        {
+            new(){Id=1,Name="Azerbaijan",Population=100000},
+			new(){Id=2,Name="Turkey",Population=222222222},
+
+        };
+		public List<City> Cities { get; set; } = new()
+		{
+			new(){Id=1,Name="Baku",CountryId=1 },
+			new(){Id=1,Name="Nakhcivan",CountryId=1 },
+			new(){Id=1,Name="Antalya",CountryId=2 },
+			new(){Id=2,Name="Izmir",CountryId=2},
+
+		};
+		public IActionResult Index()
         {
             //return Ok("Everything is okay)");
             // return Json(new { id = 1,City="Baku",Country="Azerbaijan",Population=10000000 });
             //return File("~/banner-1.png","image/png");
-            TempData["Slogan"] ="Karabakh is Azerbaijan.";
-            ViewData["Privacy"] = "Privacy is important for us..";
-            ViewBag.PhoneNumber = "+994555555555";
+            //TempData["Slogan"] ="Karabakh is Azerbaijan.";
+            //ViewData["Privacy"] = "Privacy is important for us..";
+            //ViewBag.PhoneNumber = "+994555555555";
 			//return RedirectToAction(nameof(About));
 
-			return View();
+			return View(Countries);
 		}
-		public IActionResult About()
+		public IActionResult About(int? id)
         {
-            return View();
+            if (id == null) return BadRequest();
+            var countryCities= Cities.FindAll(x => x.CountryId == id);
+            return View(countryCities);
         }
         public IActionResult Contact()
         {
